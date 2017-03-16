b54175f239f78e5c71a9f144e731a7d99a55258e
IDEY <dey.indranil@gmail.com>
2017-03-16 08:55:55 +0530

    Final version used in Expo poll

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 "# sms-s3-android" 
+This simple android app, reads sms from your phone and forwards all smss with a keyword to an s3 store.
+This was used live and worked fine.
+The aws feature which were used were 
+1. cognito identity manager
+2. s3 transfer utility
+`
+Pay some attention to the roles configuration and how cognito links up with roles and how to tie it back to the s3 bucket.
+Also note that you have to get CognitoCachingCredentialsProvider from Regions.US_EAST_1 (1 of a list of them. e.g. US_WEST_2 doesnt work.)
+
+Further to the s3 store, I had used quicksight to quickly bringup a dashboard for visualization of data which is fairly straightforward.
+Feel free to reach out to me if you have any questions.
--- app/src/main/java/com/idey/expofb4/MainActivity.java
+++ app/src/main/java/com/idey/expofb4/MainActivity.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
@@ -21,10 +23,15 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,12 +39,24 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Expo Feedback:";
     private Timer timer = new Timer();
     private Set<Integer> processedIds = new HashSet<>();
+    private Set<String> processedSenders = new HashSet<>();
     private static int cnt = 0;
+    private List<Integer> processedVals = new ArrayList<>();
+    private AtomicInteger processedCnt = new AtomicInteger(0);
+
+    TextView txtView3;
+
+    private final String NEW_LINE = System.getProperty("line.separator");
+    private final String NEW_LINE2 = "\\r\\n";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         Log.i(TAG,"Creating stuff");
+        setContentView(R.layout.activity_main);
+
+        txtView3 = (TextView) findViewById(R.id.textView3);
+
         timer.schedule(new TimerTask(){
 
             @Override
@@ -46,52 +65,90 @@ public class MainActivity extends AppCompatActivity {
                 Cursor cur = getContentResolver().query(uriSMSURI, null, null, null, null);
                 String sms = "";
                 boolean b = true;
-                Log.i(TAG,"Starting pass :"+cnt++);
+                int procSize = processedVals.size();
+                Log.i(TAG,"Starting pass :"+cnt+++",procSize:"+procSize);
+                StringBuilder sbDtl = new StringBuilder();
                 while (cur.moveToNext()) {
-            /*if (b) {
-                for (int i = 0; i < cur.getColumnCount(); i++) {
-                    sms += (cur.getColumnName(i) + ",i=" + i + "," + cur.getString(i) + "\n");
-                }
-                b = false;
-            }
-            sms += "From :" + cur.getString(2) +
-                    " : Body:" + cur.getString(11) +
-                    " : Read:" + cur.getString(6) + "\n";
-                    */
+                    String smsBody = cur.getString(12);
+                    if(smsBody==null || smsBody.trim().length()==0) continue;
                     //Log.i(TAG,"_id:"+cur.getString(0)+",value:"+cur.getString(12)+",read:"+cur.getString(7));
-                    if("0".equals(cur.getString(7))){//!read
+                    if(isExpoFb(smsBody)){//!read
                         Log.i(TAG,"processedId?"+processedIds.contains(cur.getString(0)));
                         Log.i(TAG,"_id:"+cur.getString(0)+",value:"+cur.getString(12)+",read:"+cur.getString(7));
-                        if(!processedIds.contains(cur.getString(0))){
 
-                            File tmpFile = getTmpFile(cur.getString(0),cur.getString(12));
-                            storeMsg(cur.getString(0),tmpFile);
-                            processedIds.add(Integer.valueOf(cur.getString(0)));
+                        if(!processedIds.contains(Integer.valueOf(cur.getString(0)))){
+                            if(!processedSenders.contains(cur.getString(2))) {
+                                processedVals.add(sanitizedInput(smsBody));
+                                processedIds.add(Integer.valueOf(cur.getString(0)));
+                                processedSenders.add(cur.getString(2));
+                            }
+                            sbDtl.append(cur.getString(2)).append(",").append(smsBody).append(NEW_LINE);
                         }
-
+                        processedCnt.set(processedVals.size());
                     }
                 }
-
+                Log.i(TAG,"Ending pass :"+cnt+++",procSize:"+procSize+",procVals.size():"+processedVals.size());
+                if(procSize!=processedVals.size()) {
+                    File tmpFile = getTmpFile("expofb", createCsv(processedVals));
+                    storeMsg("expofb",tmpFile);
+                    File tmpFileDtl = getTmpFile("expofbdtl", sbDtl.toString());
+                    storeMsg("expofbdtl",tmpFileDtl);
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        updateTextView();
+                    }
+                });
             }
 
             }
         },10000,120000);
     }
-    public void storeMsg(String key, File contentFile){
+
+    private void updateTextView() {
+        txtView3.setText(Integer.toString(processedCnt.get()));
+    }
+
+    private String createCsv(List<Integer> processedVals) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Votes").append(NEW_LINE);
+        for(int i:processedVals){
+            sb.append(i).append(NEW_LINE);
+        }
+        return sb.toString();
+    }
+
+    private Integer sanitizedInput(String smsBody) {
+        String remBody = smsBody.replaceAll("(?i)wmexpofb","");
+        Integer ret = 0;
+        try {
+            ret = Integer.valueOf(remBody.trim());
+        } catch (Exception e){
+            Log.e(TAG,e.getMessage());
+        }
+        return ret;
+    }
+
+    private boolean isExpoFb(String smsBody) {
+        return smsBody!=null && smsBody.toLowerCase().contains("wmexpofb");
+    }
+
+    public void storeMsg(String key,File contentFile){
 
         AmazonS3 s3 = new AmazonS3Client(getCredentialsProvider());
         TransferUtility transferUtility = new TransferUtility(s3, getApplicationContext());
-        ObjectMetadata myObjectMetadata = new ObjectMetadata();
 
         TransferObserver observer = transferUtility.upload(
                 MY_BUCKET,     /* The bucket to upload to */
-                key,    /* The key for the uploaded object */
+                key+".csv",    /* The key for the uploaded object */
                 contentFile        /* The file where the data to upload exists */
         );
         
         observer.setTransferListener(new TransferListener(){
             public void onProgressChanged(int id, long bytesCurrent, long bytesTotal){
                 Log.i(TAG,"id:"+id+",bytesCurrent:"+bytesCurrent+",bytesTotal:"+bytesTotal);
+
             }
             public void onStateChanged(int id, TransferState state){
                 Log.i(TAG, "id:"+id+",state:"+state);
@@ -118,7 +175,7 @@ public class MainActivity extends AppCompatActivity {
         File outputDir = getApplicationContext().getCacheDir(); // context being the Activity pointer
         File outputFile = null;
         try{
-            outputFile = File.createTempFile(key, "tmp", outputDir);
+            outputFile = File.createTempFile(key, ".tmp", outputDir);
             BufferedWriter bw = null;
             try{
                 bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFile)));
@@ -140,7 +197,7 @@ public class MainActivity extends AppCompatActivity {
         // Initialize the Amazon Cognito credentials provider
         CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
                 getApplicationContext(),
-                "us-east-1:61e75f65-e9e7-4567-b915-xxxxxxxxxxxx", // Identity Pool ID
+                "us-east-1:61e75f65-e9e7-4567-b915-xxxxxxxxxxx", // Identity Pool ID
                 Regions.US_EAST_1 // Region
         );
         return credentialsProvider;
